2b0c63ef316afd5a2b11970e9408c1a1981a9901
LokeshN1 <lokeshnegi399@gmail.com>
2023-11-14 19:01:22 +0530

    Installed mysql2 package also conect the database with node.

--- index.js
+++ index.js
@@ -3,6 +3,30 @@ const app = express();
 
 const port = 8080;
 
+// Requireing MySql2 Database -:
+const mysql = require('mysql2');        // get the client
+
+// create the connection to database-:
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'Inventory',
+  password: "L1oke&hn1sql"
+});
+
+
+let q = "SHOW TABLES";
+
+// Query -:
+try {
+    connection.query(q, (err, result) =>{
+        if(err) throw err;
+        console.log(result);
+    });
+} catch (err) {
+    console.log(err);
+}
+
 app.listen(port, (req, res) =>{     // checking server connection
     console.log(`Server is Listening on Port : ${port}`);
 })

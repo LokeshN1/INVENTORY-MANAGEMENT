fe2414732523889f675c177c51016d881354beef
LokeshN1 <lokeshnegi399@gmail.com>
2023-11-16 13:57:22 +0530

    Added 'Update Inventory' button in home page inside the update page 'Edit' and 'Delete' button is provided. You can edit inventory data.

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const {v4: uuidv4} = require('uuid');
 // Requiring 'method-override' package-:
 const methodOverride = require("method-override");
 app.use(methodOverride('_method'));
+app.use(express.urlencoded({extended: true}));      // so express can read data came from form
 
 const port = 8080;
 
@@ -30,6 +31,7 @@ const connection = mysql.createConnection({
 
 let q = "SHOW TABLES";
 
+
 // Query -:
 try {
     connection.query(q, (err, result) =>{
@@ -45,20 +47,89 @@ app.listen(port, (req, res) =>{     // checking server connection
     console.log(`Server is Listening on Port : ${port}`);
 })
 
+
+// HOME PAGE 
 app.get("/home",(req, res)=>{
     res.render("home.ejs");
 })
 
+
+// SHOW INVENTORY DATA-: 
 app.get("/home/show", (req, res) =>{
     let q = `SELECT * FROM list`;
     try {
         connection.query(q, (err, list_data) =>{
             console.log(list_data);
-            // res.
             res.render("show.ejs", {list_data});
         })
     } catch (err) {
         console.log(err);
+        res.send("Database facing Error. Try Again...");
     }
-    // res.send("dfkjs");
+})
+
+
+// Update Page-: here 'edit' and 'delete' button will be provided along with inventory data
+app.get("/home/update", (req, res) =>{
+    let q = `SELECT * FROM list`;
+    try {
+        connection.query(q, (err, list_data) =>{
+            console.log(list_data);
+
+            res.render("update.ejs", {list_data});      // redering to update page
+        })
+    } catch (err) {
+        console.log(err);
+        res.send("Database facing Error. Try Again...");
+    }
+})
+
+
+// Edit Page-: here we will take new value from the user which user want to insert
+app.get("/update/:id/edit", (req, res)=>{
+    let {id : cur_id} = req.params;
+    console.log(cur_id);
+    let q = `SELECT * FROM list WHERE id = '${cur_id}'`;
+    try {
+        connection.query(q, (err, result)=>{
+            if(err) throw err;
+            console.log(result);
+            let edit_product = result[0];
+            console.log(edit_product);
+            res.render("edit.ejs", {edit_product});     // rendering to edit page
+
+        })
+    } catch (err) {
+        console.log(err);
+        res.send("Database facing Error. Try Again...");
+    }
+})
+
+
+// Updating inventory-: after taking updated data from edit page patch request will sent.
+app.patch("/edit/:id", (req, res) =>{
+    console.log(req.params);
+    let {id : id} = req.params;
+
+    console.log(req.body);      // updated data is stored on req.body
+
+    let {unit : new_unit, ppu : new_ppu} = req.body;
+    let new_stock_value = new_unit*new_ppu;
+
+    // updating data of database-:
+    q = `UPDATE list SET
+        unit = ${new_unit},
+        ppu = ${new_ppu},
+        stock_value = ${new_stock_value}
+        WHERE id = '${id}'`;
+
+    try {
+        connection.query(q, (err, result) => {
+            res.redirect("http://localhost:8080/home/show");    // after updating data redirecting to show page{where all inventory data will shown}
+        })
+    } catch (err) {
+        console.log(err);
+        res.send("Updation Unsuccessful");
+    }
+
 })
